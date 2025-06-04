e89725a7de3df4647da0c09e17f90b7c122fbc88
Brenden C Edwards <edwardsbrenden@gmail.com>
2025-06-03 22:09:59 -0500

    V 5.0.0

--- src/carddata.jsx
+++ src/carddata.jsx
@@ -2,23 +2,75 @@ import React from 'react';
 
 const cardData = [
     {
-        name: "Austin the Great",
+        name: "Austin The Great",
         type: "Creature",
         cost: "3 Mana",
         power: "3",
         toughness: "5",
-        description: "'Austin the Great, was a conqueror of the world. He was a great leader and a great warrior. He was a great king. He was a great emperor. He was a great pharaoh. He was a great king of Egypt.'",
+        keywords: "Trample, Haste",
+        description: "Austin the Great, ruler of the disc golf. Played many games. Surpassed Brenden in StarWars Game a feat done by no one else.",
         image: "/images/Austin.webp"
     },
     {
-        name: "Nick the Great",
+        name: "Nick The All Powerful",
         type: "Creature",
         cost: "4 Mana",
         power: "1",
         toughness: "1",
-        description: "This is the second card",
+        keywords: "Flying, Reach",
+        description: "Nick the all powerful, the son of the great Austin the Great. He is the greatest of all time. Slayer of many, many foes. He is the greatest of all time.",
         image: "/images/Nicks.webp"
     },
+    {
+        name: "Brandon The Wise",
+        type: "Creature",
+        cost: "3 Mana",
+        power: "3",
+        toughness: "5",
+        keywords: "Deathtouch, Hexproof",
+        description: "Brandon the wise is a great leader of the league of legends. A twitch aram main, forced to teach the next generation of players how to play the game.",
+        image: "/images/Bandon.webp"
+    },
+    {
+        name: "Jayson Luckyfeet",
+        type: "Creature",
+        cost: "3 Mana",
+        power: "3",
+        toughness: "1",
+        keywords: "",
+        description: "Jayson Luckyfeet answers not to anyone, yet retains luck immeasurable. He is the luckiest of all time.",
+        image: "/images/Jayson.png"
+    },
+    {
+        name: "Thyago The Coder",
+        type: "Creature",
+        cost: "2 Mana",
+        power: "4",
+        toughness: "4",
+        keywords: "Hexproof",
+        description: "Thyago The Coder is a great coder. He is the friend of the best coder in the world, Pedro the electric Blanca.",
+        image: "/images/Thyago.png"
+    },
+    {
+        name: "Pedro the electric Blanca",
+        type: "Creature",
+        cost: "3 Mana",
+        power: "3",
+        toughness: "5",
+        keywords: "Reach, Haste",
+        description: "Pedro the electric Blanca is a great coder. He is the best coder in the world. He is the greatest of all time maybe even better than Thyago.",
+        image: "/images/Pedro.png"
+    },
+    {
+        name: "Brenden The Comsmic Coder",
+        type: "Creature",
+        cost: "3 Mana",
+        power: "3",
+        toughness: "5",
+        keywords: "Reach, Haste",
+        description: "Brenden The Comsmic Coder is a great coder. He is the best coder in the world. He is the greatest of all time maybe even better than Pedro.",
+        image: "/images/Brenden.png"
+    }
 ]
 
 export default cardData;
--- src/components/cards/card.jsx
+++ src/components/cards/card.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import './cardcss.css';
 
+// Keyword descriptions for tooltips
+const keywordDescriptions = {
+    Trample: "If this creature would assign enough damage to its blockers to destroy them, it can assign the rest of its damage to the player or planeswalker it's attacking.",
+    Haste: "This creature can attack as soon as it comes under your control.",
+    Flying: "This creature can't be blocked except by creatures with flying or reach.",
+    Reach: "This creature can block creatures with flying.",
+    Hexproof: "This creature can't be affected by spells or abilities that target creatures.",
+    Deathtouch: "Any creature will die if attacking or blocking this creature.",
+    // Add more as needed
+};
+
 const Card = ({ info, isFlipped }) => {
     return (
         <div className={`card-3d-container`}>
@@ -16,6 +27,16 @@ const Card = ({ info, isFlipped }) => {
                     <div className="card-description">
                         <h3 className="card-description-type">{info.type}</h3>
                         <h3 className="card-description-text">{info.description}</h3>
+                        <div className="card-description-keywords">
+                            {info.keywords && info.keywords.split(',').map((kw, i) => {
+                                const keyword = kw.trim();
+                                return (
+                                    <abbr key={keyword} title={keywordDescriptions[keyword] || keyword} style={{cursor: 'help', marginRight: 8}}>
+                                        {keyword}
+                                    </abbr>
+                                );
+                            })}
+                        </div>
                     </div>
                     <div className="card-footer">
                         <h5 className="card-footer-text">{info.power}/{info.toughness}</h5>
